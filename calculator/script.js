3f4c8993bffa0654fb66c7b6abf2f6ca2e2cbe59
wersosnwsk <wersosnwsk@gmail.com>
2024-07-23 14:48:58 +0200

    calc without js

--- calculator/script.js
+++ calculator/script.js
@@ -1,1 +1,34 @@
-//Script
+//Script:
+let x, result = 0;
+let buffor = "0";
+
+function addNum(x) {
+    result += x;
+    return result;
+}
+
+function subNum(x) {
+    result -=x;
+    return result;
+}
+
+function multiNum(x) {
+    result *= x;
+    return result;
+}
+
+function diviNum(x) {
+    result /= x;
+    return result;
+}
+
+function button(value) {
+    switch(value) {
+        case '+':
+            addNum(value);
+            break;
+        default:
+            result = 0;
+            break;
+    }
+}
