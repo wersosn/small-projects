14474457394ada17825a2645d5a17ac47a373977
wersosnwsk <wersosnwsk@gmail.com>
2024-07-23 17:54:17 +0200

    some functionality

--- calculator/script.js
+++ calculator/script.js
@@ -1,38 +1,105 @@
-//Script:
-let x, result = 0;
+//Variables:
+let result = 0, operator, equal;
 let buffor = "0";
+const number = document.querySelector('.result');
 
-function addNum(x) {
-    result += x;
-    return result;
+//Function to handle clicking buttons:
+function click(value) {
+    if(isNaN(value)) {
+        symbols(value);
+    }
+    else {
+        numb(value);
+    }
+    number.innerText = buffor;
 }
 
-function subNum(x) {
-    result -=x;
-    return result;
-}
-
-function multiNum(x) {
-    result *= x;
-    return result;
-}
-
-function diviNum(x) {
-    result /= x;
-    return result;
-}
-
-/*function f() {
-    document.getElementById("num").innerHTML = "Liczba";
-}*/
-
-function button(value) {
-    switch(value) {
+//Function to read and handle symbols:
+function symbols(symbol) {
+    let numberValue = parseInt(buffor);
+    switch(symbol) {
         case '+':
-            addNum(value);
+        case '−':
+        case '×':
+        case '÷':
+        case '%':
+            if(result === 0) {
+                result = numberValue;
+            }
+            else {
+                calc(numberValue);
+            }
+            operator = symbol;
+            buffor = '0';
+            break;
+        case 'C':
+            buffor = '0';
+            result = 0;
+            break;
+        case '=':
+            if(operator === null) {
+                return 0;
+            }
+            equal = parseInt(buffor);
+            calc(equal);
+            operator = null;
+            buffor = result.toString();
+            break;
+        case '←':
+            if(buffor.length < 1) {
+                buffor = '0';
+            }
+            else {
+                buffor.slice(0, buffor.length - 1);
+            }
             break;
         default:
             result = 0;
+            buffor = '0';
             break;
     }
 }
+
+//Function to handle strings:
+function numb(string) {
+    if(buffor === '0') {
+        buffor = string;
+    }
+    else {
+        buffor += string;
+    }
+}
+
+//Function to calculate result:
+function calc(numberValue) {
+    switch(operator) {
+        case '+':
+            result += numberValue;
+            break;
+        case '−':
+            result -= numberValue;
+            break;
+        case '×':
+            result *= numberValue;
+            break;
+        case '÷':
+            result /= numberValue;
+            break;
+        case '%':
+            result %= numberValue;
+            break;
+        //to be added - other operators
+        default:
+            result = numberValue;
+            break;
+    }
+}
+
+//Initialization of the function:
+function f() {
+    document.querySelector('.buttons').addEventListener('click', function(event) { click(event.target.innerText) });
+}
+
+f();
+
+//to be added - different way of displaying result etc.
